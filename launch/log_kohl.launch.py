8a653bbda788be939d031069780d8002e83d8167
Johannes Hackbarth <j.hackbarth@cubegaming.de>
2023-02-17 09:56:14 +0000

    Add raw data log launchfile for kohl tour

---
+++ launch/log_kohl.launch.py
@@ -0,0 +1,31 @@
+import launch
+
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        launch.actions.ExecuteProcess(
+            cmd=['ros2', 'bag', 'record', 
+                 '/camera_info',
+                 '/color/video/image',
+                 'color/video/camera_info',
+                 '/cmd_vel',
+                 '/fix',
+                 '/head_mcu',
+                 '/heading',
+                 '/hoverboard_api',
+                 '/image_raw',
+                 '/joint_states',
+                 '/joy',
+                 '/odom',
+                 '/robot_description',
+                 '/pilsbot_velocity_controller/cmd_vel',
+                 '/pilsbot_velocity_controller/cmd_vel_out',
+                 '/stereo/depth',
+                 '/stereo/camera_info'
+                 '/tf',
+                 '/tf_static',
+                 '/time_reference',
+                 '/vel'],
+            output='screen'
+        )
+    ])
